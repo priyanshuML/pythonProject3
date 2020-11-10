f095016c48bfd3a499454a093a2b680562acfa40
priyanshuML <42387689+priyanshuML@users.noreply.github.com>
2020-11-10 20:02:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi this is the the, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi this is the the what, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
